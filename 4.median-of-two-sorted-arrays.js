8a69a6ae294bfa2ac1f5d085f291ac6f2f62bab3
gaoting <837082729@qq.com>
2023-12-05 19:42:47 +0800

    feat: 我真👍🏻

--- 4.median-of-two-sorted-arrays.js
+++ 4.median-of-two-sorted-arrays.js
@@ -5,7 +5,6 @@
  * [4] 寻找两个正序数组的中位数
  */
 
-
 // @lcpr-template-start
 
 // @lcpr-template-end
@@ -15,12 +14,14 @@
  * @param {number[]} nums2
  * @return {number}
  */
-var findMedianSortedArrays = function(nums1, nums2) {
-
+var findMedianSortedArrays = function (nums1, nums2) {
+  const arr = [...nums1, ...nums2];
+  arr.sort((a, b) => a - b);
+  return arr.length % 2 === 1
+    ? arr[(arr.length - 1) / 2].toFixed(5)
+    : ((arr[arr.length / 2 - 1] + arr[arr.length / 2]) / 2).toFixed(5);
 };
 // @lc code=end
-
-
 
 /*
 // @lcpr case=start
@@ -32,4 +33,3 @@ var findMedianSortedArrays = function(nums1, nums2) {
 // @lcpr case=end
 
  */
-
